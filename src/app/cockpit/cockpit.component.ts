a0457537656bc980c34a787fa7784699b0aad938
Michael Haymon <michael.haymon@ruralsource.com>
2019-11-13 14:34:15 -0600

    afternoon commit

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -7,34 +7,40 @@ import {
   ElementRef,
   OnChanges,
   SimpleChanges
-} from '@angular/core';
+} from "@angular/core";
 
 @Component({
-  selector: 'app-cockpit',
-  templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  selector: "app-cockpit",
+  templateUrl: "./cockpit.component.html",
+  styleUrls: ["./cockpit.component.css"]
 })
 export class CockpitComponent implements OnInit, OnChanges {
-  @Output() serverCreated = new EventEmitter<{ serverName: string; serverContent: string }>();
-  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string; serverContent: string }>();
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  @Output("bpCreated") blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
   // newServerName = '';
   // newServerContent = '';`
-  @ViewChild('serverContentInput', { static: true }) serverContentInput: ElementRef;
+  @ViewChild("serverContentInput", { static: true })
+  serverContentInput: ElementRef;
 
   constructor() {
-    console.log('Constructor Called');
+    console.log("Constructor Called");
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log('ngOnChanges Called');
+    console.log("ngOnChanges Called");
   }
 
   ngOnInit() {
-    console.log('ngOnInit Called');
+    console.log("ngOnInit Called");
   }
 
   onAddServer(nameInput: HTMLInputElement, contentInput: HTMLInputElement) {
-    console.log(this.serverContentInput);
     this.serverCreated.emit({
       serverName: nameInput.value,
       serverContent: this.serverContentInput.nativeElement.value
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-server-element',
-  templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  selector: "app-server-element",
+  templateUrl: "./server-element.component.html",
+  styleUrls: ["./server-element.component.css"]
 })
 export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: { type: string; name: string; content: string };
+  @Input("srvElement") element: { type: string; name: string; content: string };
 
   constructor() {}
 
