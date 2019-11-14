584947bdedeb5c6f40965529b8ea58090b9b6f1b
michaelhaymon <michael.haymon@ruralsource.com>
2019-11-14 09:44:38 -0600

    tslint commit

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
-  selector: "app-server-element",
-  templateUrl: "./server-element.component.html",
-  styleUrls: ["./server-element.component.css"]
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input("srvElement") element: { type: string; name: string; content: string };
+  @Input('srvElement') element: { type: string; name: string; content: string };
 
-  constructor() { }
+  constructor() {
+    console.log('Constructor Called!');
+  }
 
-  ngOnInit() { }
+  ngOnChanges() {
+    console.log('Onchanges called!');
+  }
+
+  ngOnInit() {
+    console.log('OnInit Called!');
+  }
 }
